953b4426bc192e77d8b48006e62cbd1ce556c94c
jitendrahellofy <jitendra.hellofy@gmail.com>
2025-07-04 10:21:26 +0530

    Remove .next from git tracking

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,17 +1,32 @@
+'use client';
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 type CardProps = {
   title: string;
   description: string;
   icon?: React.ReactNode;
+  href?: string;
 };
 
-const Card: React.FC<CardProps> = ({ title, description, icon }) => {
+const Card: React.FC<CardProps> = ({ title, description, icon, href }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (href) router.push(href);
+  };
+
   return (
-    <div className="w-full border-lightgray border-2 rounded-lg p-6 hover:">
-      <div className="bg-aliceblue h-[40px] w-[40px] flex items-center justify-center rounded">{icon}</div>
-        <h3 className="text-base font-semibold text-primary mt-2 mb-1">{title}</h3>
-        <p className="text-secondary text-sm font-normal mb-0">{description}</p>
+    <div
+      onClick={handleClick}
+      className={`w-full border-2 rounded-lg p-6  ${href ? 'cursor-pointer' : ''}`}
+    >
+      <div className="bg-aliceblue h-[40px] w-[40px] flex items-center justify-center rounded">
+        {icon}
+      </div>
+      <h3 className="text-base font-semibold text-primary mt-2 mb-1">{title}</h3>
+      <p className="text-secondary text-sm font-normal mb-0">{description}</p>
     </div>
   );
 };
