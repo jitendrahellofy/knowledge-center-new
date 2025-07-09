9063ea1d6414e372822cb222426d0b7b9682714b
jitendrahellofy <jitendra.hellofy@gmail.com>
2025-07-09 18:37:00 +0530

    knowledgecenter ui update for search & add templates module.

--- components/Card.tsx
+++ components/Card.tsx
@@ -20,12 +20,12 @@ const Card: React.FC<CardProps> = ({ title, description, icon, href }) => {
   return (
     <div
       onClick={handleClick}
-      className={`w-full border-2 rounded-lg p-6  ${href ? 'cursor-pointer' : ''}`}
+      className={`w-full border-2 rounded-lg p-6 hover:border-fd-primary group transition-all duration-300  ${href ? 'cursor-pointer' : ''}`}
     >
       <div className="bg-aliceblue h-[40px] w-[40px] flex items-center justify-center rounded">
         {icon}
       </div>
-      <h3 className="text-base font-semibold text-primary mt-2 mb-1">{title}</h3>
+      <h3 className="text-base font-semibold text-primary mt-2 mb-1 group-hover:text-fd-primary">{title}</h3>
       <p className="text-secondary text-sm font-normal mb-0">{description}</p>
     </div>
   );
